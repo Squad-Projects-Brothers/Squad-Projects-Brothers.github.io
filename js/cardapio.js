55b96e1a459917782c62b55178af962b00062b3c
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-07-24 23:26:24 -0300

    adicionando função no botão editar

--- js/cardapio.js
+++ js/cardapio.js
@@ -1,7 +1,7 @@
 
 function pegaritens() {
     url = "https://ravinristorant.000webhostapp.com/json/server-itens.php";
-   // url = "http://localhost/Squad-Projects-Brothers.github.io/json/server-itens.php";
+    // url = "http://localhost/Squad-Projects-Brothers.github.io/json/server-itens.php";
 
     fetch(url)
         .then((dados) => {
@@ -52,31 +52,71 @@ function removeNumeroDeItens() {
     if (currentValue > 0) {
         numberItem.innerText = currentValue - 1;
     }
-}/*
-function addItensCarrinho() {
-    const carrinhoDeCompras = [];
+}
+var itemEmEdicao = null; // Variável para armazenar o item em edição
 
-    function adicionarItemCarrinho(item) {
-      carrinhoDeCompras.push(item);
-      atualizarListaCarrinho();
-    }
-
-    function atualizarListaCarrinho() {
-      const listaCarrinho = document.getElementById("listaCarrinho");
-      listaCarrinho.innerHTML = ""; 
-
-      carrinhoDeCompras.forEach(item => {
-        const listItem = document.createElement("li");
-        listItem.textContent = item;
-        listaCarrinho.appendChild(listItem);
-      });
-    }
-}*/
-function removerDiv(button) {
+function editarDiv(button) {
     // Obtém o elemento pai do botão clicado, que é a div com a classe "restaurant-cart-item sidebar-pedido-line"
-    var divItem = button.closest(".restaurant-cart-item");
-    
+    var divItem = button.closest(".sidebar-pedido-line");
+    console.log(divItem);
+
+    // Verifica se o elemento pai foi encontrado corretamente
+    if (!divItem) {
+        console.error("Elemento com a classe 'restaurant-cart-item' não encontrado.");
+        return;
+    }
+
+    // Obtém o elemento filho com a classe "sidebar-pedido-item-description"
+    var itemDescription = divItem.querySelector(".sidebar-pedido-item-description");
+
+    // Verifica se o elemento filho foi encontrado corretamente
+    if (!itemDescription) {
+        console.error("Elemento com a classe 'sidebar-pedido-item-description' não encontrado dentro de 'restaurant-cart-item'.");
+        return;
+    }
+
+    // Obtém o texto do item do pedido e o preço
+    var itemTexto = itemDescription.querySelector("span:first-child").innerText;
+    var itemPreco = itemDescription.querySelector("span:last-child").innerText;
+
+    // Preenche o formulário de edição com os detalhes do item
+    document.getElementById("quantidade").value = parseInt(itemTexto);
+
+    // Exibe o formulário de edição
+    document.querySelector(".modal-form-edit").style.display = "block";
+
+    // Armazena o item em edição para posterior atualização
+    itemEmEdicao = divItem;
+}
+
+function salvarEdicao() {
+    var novaQuantidade = document.getElementById("quantidade").value;
+
+    // Atualiza o texto do item com a nova quantidade
+    var descricaoItem = itemEmEdicao.querySelector(".sidebar-pedido-item-description span:first-child");
+    descricaoItem.innerText = novaQuantidade + "x Chopp Pet 1litro";
+
+    // Fecha o formulário de edição
+    document.querySelector(".modal-form-edit").style.display = "none";
+
+    // Limpa a variável de item em edição
+    itemEmEdicao = null;
+}
+
+function cancelarEdicao() {
+    // Fecha o formulário de edição
+    document.querySelector(".modal-form-edit").style.display = "none";
+
+    // Limpa a variável de item em edição
+    itemEmEdicao = null;
+}
+
+function removerDiv(button) {
+    // Obtém o elemento pai do botão clicado, que é a div com a classe "sidebar-pedido-item"
+    var divItem = button.closest(".sidebar-pedido-item");
+
     // Remove a div inteira do DOM
     divItem.remove();
 }
+
 
