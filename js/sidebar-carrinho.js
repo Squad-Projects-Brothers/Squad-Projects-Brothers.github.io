cf998bb345ea2e084813dc356339393fdd531163
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-07-27 22:26:46 -0300

    adicionando a função no carrinho
    Adicionado função de adicionar produto no carrinho

--- js/sidebar-carrinho.js
+++ js/sidebar-carrinho.js
@@ -58,32 +58,35 @@ function fecharModel() {
 //função acionada ao clicar no botao adicionar produto no model de confirmação
 function adicionarProdutoNoCarrinho(id, nome, categoria, descricao, valor) {
     let modalItem = document.getElementById('modalItem1');
-    let valorItem = document.getElementById("valorItem").innerText;
+    let valorItem = document.getElementById("valorItem").textContent.replace("R$ ", "").replace(",", ".");
     let numberItem = document.getElementById("numberItem").innerText;
     let nomeProd = document.getElementById("modalItem1Label").innerText;
     let listaItens = document.getElementById("listaItens");
-    let quantidadeItens = document.getElementById("numberItem").innerText;
-    let valorFinal = (parseInt(quantidadeItens) * parseFloat(valor))
+    let quantidadeItens = parseInt(numberItem);
+
+    let valorFinal = (parseFloat(quantidadeItens) * parseFloat(valorItem.substring(2)));
+    console.log(valorFinal, valorItem +' 1')
+    // Verificar se o item já existe na lista
     let itemJaExistente = false;
     let itensNaLista = listaItens.getElementsByClassName('restaurant-cart-item');
 
     for (let i = 0; i < itensNaLista.length; i++) {
         let item = itensNaLista[i];
         let nomeItem = item.getElementsByTagName('span')[1].innerText;
-        
+       
         if (nomeItem === nomeProd) {
             // O item já existe na lista, atualize a quantidade e o valor
-            let quantidadeSpan = item.getElementsByTagName('span')[0].innerHTML;
-            let valorSpan = item.getElementsByTagName('span')[2].innerHTML;
-            console.log(valorSpan)
-            console.log(quantidadeSpan)
+            let quantidadeSpan = item.getElementsByTagName('span')[0].innerText;
+            console.log(quantidadeSpan + ' 3')
+            console.log(item.getElementsByTagName('span')[0].innerText)
+            let valorSpan = parseFloat(item.getElementsByTagName('span')[2].innerText.substring(2));
+            console.log(valorSpan + ' 2')
             let quantidadeExistente = parseInt(quantidadeSpan.innerText);
-            let valorExistente = parseFloat(valorSpan.innerText.replace("R$ ", ""));
+            let valorExistente = parseFloat(valorSpan.innerText);
 
             let novaQuantidade = quantidadeExistente + quantidadeItens;
-            let novoValor = (novaQuantidade * parseFloat(valorItem));
-            console.log(novaQuantidade)
-            console.log(novoValor)
+            let novoValor = (novaQuantidade * parseFloat(valorItem.substring(2)));
+
             quantidadeSpan.innerText = novaQuantidade;
             valorSpan.innerText = "R$ " + novoValor.toFixed(2);
 
@@ -95,51 +98,51 @@ function adicionarProdutoNoCarrinho(id, nome, categoria, descricao, valor) {
     // Se o item não existe na lista, adicione um novo item
     if (!itemJaExistente) {
         let textHtml = `
-        <div class="restaurant-cart-item sidebar-pedido-line">
-            <div class="sidebar-pedido-item-description sidebar-pedido-justify">
-                <span>${quantidadeItens}</span>
-                <span>${nomeProd}</span><span>R$ ${valorFinal.toFixed(2)}</span>
-            </div>
-            <div class="sidebar-pedido-item-tags"></div>
-            <div class="sidebar-pedido-item-buttons-wrapper">
-                <button type="button"
-                    role="button"
-                    class="btn btn-link-edit btn-size-m sidebar-pedido-button-item-button"
-                    theme="link" variant="sidebar-pedido-button-item-button" label="Editar"
-                    data-test-id="restaurant-cart-item__edit-button" color="primary"
-                    data-bs-toggle="modal" data-bs-target="#modalItem1">
-                    <span class="btn-label">Editar</span>
-                </button>
-                <button type="button"
-                    role="button"
-                    class="btn btn-link-edit btn-gray btn-size-m sidebar-pedido-button-item-button"
-                    theme="link" color="gray" variant="sidebar-pedido-button-item-button"
-                    label="Remover" data-test-id="restaurant-cart-item__remove-button"
-                    target="" rel="" onclick="removerDiv(this)">
-                    <span class="btn-label">Remover</span>
-                </button>
-            </div>
-        </div>`;
+    <div class="restaurant-cart-item sidebar-pedido-line">
+        <div class="sidebar-pedido-item-description sidebar-pedido-justify">
+            <span>${quantidadeItens}</span>
+            <span>${nomeProd}</span><span>R$ ${valorFinal.toFixed(2)}</span>
+        </div>
+        <div class="sidebar-pedido-item-tags"></div>
+        <div class="sidebar-pedido-item-buttons-wrapper">
+            <button type="button"
+                role="button"
+                class="btn btn-link-edit btn-size-m sidebar-pedido-button-item-button"
+                theme="link" variant="sidebar-pedido-button-item-button" label="Editar"
+                data-test-id="restaurant-cart-item__edit-button" color="primary"
+                data-bs-toggle="modal" data-bs-target="#modalItem1">
+                <span class="btn-label">Editar</span>
+            </button>
+            <button type="button"
+                role="button"
+                class="btn btn-link-edit btn-gray btn-size-m sidebar-pedido-button-item-button"
+                theme="link" color="gray" variant="sidebar-pedido-button-item-button"
+                label="Remover" data-test-id="restaurant-cart-item__remove-button"
+                target="" rel="" onclick="removerDiv(this)">
+                <span class="btn-label">Remover</span>
+            </button>
+        </div>
+    </div>`;
 
         listaItens.innerHTML += textHtml;
     }
 }
 
-//função de mudar quantidade de produtos no model confirmação
-function addNumeroDeItens() {
-    let numberItem = document.getElementById("numberItem");
-    let currentValue = parseInt(numberItem.innerText);
-    numberItem.innerHTML = currentValue + 1;
+    //função de mudar quantidade de produtos no model confirmação
+    function addNumeroDeItens() {
+        let numberItem = document.getElementById("numberItem");
+        let currentValue = parseInt(numberItem.innerText);
+        numberItem.innerHTML = currentValue + 1;
 
-}
-//função de mudar quantidade de produtos no mdoel confirmação
-function removeNumeroDeItens() {
-    let numberItem = document.getElementById("numberItem");
-    let currentValue = parseInt(numberItem.innerText);
-    if (currentValue > 0) {
-        numberItem.innerText = currentValue - 1;
     }
-}/*
+    //função de mudar quantidade de produtos no mdoel confirmação
+    function removeNumeroDeItens() {
+        let numberItem = document.getElementById("numberItem");
+        let currentValue = parseInt(numberItem.innerText);
+        if (currentValue > 0) {
+            numberItem.innerText = currentValue - 1;
+        }
+    }/*
 function salvarUltimoPedido(pedido) {
     localStorage.setItem("lastOrder", JSON.stringify(pedido));
 }
