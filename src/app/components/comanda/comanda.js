26e34738c9ef89ef0b8788dee9a3421b00a6214f
kaue ochner <79806265+kakoch@users.noreply.github.com>
2023-08-09 21:50:18 -0300

    adicionando função na comanda
    Adicionando primeiro escopo da função de trazer os pedidos na aba comanda

--- src/app/components/comanda/comanda.js
+++ src/app/components/comanda/comanda.js
@@ -1,4 +1,33 @@
-function receberPedido(){
-     
+function receberPedido() {
+    //var socket = io("http://ravin.ddns.net:3001");
+    var socket = io("http://187.85.120.71:3001");
 
+    socket.on("itemsArray", (itemm) => {
+        // Limpa o conteúdo atual do elemento com a classe "messages"
+        $(".messages").empty();
+
+        return addNewComanda(itemm);
+    });
+}
+
+function addNewComanda(itemm) {
+    //const codigoMesa = localStorage.get('mesaEscolhida')
+    itemm.forEach((item) => {
+        const listaPedidos = document.getElementById("lista_pedidos");
+        const produtos = item.produtos;
+        produtos.forEach((produto) => {
+            let textHtml = `
+                <tr>
+                    <td>${produto.id}</td>
+                    <td>${item.produtos.mesa}</td>
+                    <td>${produto.nome}</td>
+                    <td>Kg</td>
+                    <td>${produto.quantidade}</td>
+                    <td>R$ 10.00</td>
+                    <td>Pago</td>
+                    <td>${produto.status}</td>
+                </tr>`;
+            listaPedidos.innerHTML += textHtml;
+        });
+    });
 }
